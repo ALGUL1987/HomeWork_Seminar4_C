cb0950a35b39bc4dad101579af419d16e04e274e
ALGUL1987 <tankipanki1@gmail.com>
2023-02-23 20:09:30 +0300

    finish

---
+++ 002/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+Console.WriteLine("Введите число");
+string num = Console.ReadLine();
+
+int SumValue(string num)
+{
+    int sum = 0;
+    int value;
+    if (Convert.ToInt32(num) >= 0)
+    {
+        for (int i = 0; i < num.Length; i++)
+        {
+            value = int.Parse(num[i].ToString());
+            sum = sum + value;
+        }
+    }
+    else
+    for (int i = 1; i < num.Length; i++)
+        {
+            value = int.Parse(num[i].ToString());
+            sum = sum + value;
+        }
+    return sum;
+}
+Console.WriteLine($"Сумма цифр в числе {num} равна {SumValue(num)}");
---
+++ 003/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] GetBinaryArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Введите число: ");
+        array[i]= Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int[] resultArray = GetBinaryArray(8);
+Console.WriteLine($"Массив: [{String.Join("\t", resultArray)}]");
